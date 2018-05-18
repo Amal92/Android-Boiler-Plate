34f93fbe8099eeab321d6d3f816a882cc28a2879
amal <amalpaul0492@gmail.com>
2018-05-18 11:38:04 +0530

    initial commit

--- app/src/main/java/com/paul/cruz/Utils/Endpoints.java
+++ app/src/main/java/com/paul/cruz/Utils/Endpoints.java
@@ -4,7 +4,7 @@ package com.paul.cruz.Utils;
  * Created by amal on 13/10/17.
  */
 public class Endpoints {
-    public static String BASE_URL = "http://34.214.130.192/";
+    public static String BASE_URL = "http://endpoint.com/";
     public static String LOGIN_URL = BASE_URL + "oauth/token";
     public static String REGISTER_URL = BASE_URL + "api/user/signup";
     public static String FORGOT_PASSWORD_URL = BASE_URL + "api/user/forgot/password";
